594db68a1e30520c4d1cb6dc70c9b1fc1a47ebca
Aayush Vetwal <aayushvetwal@outlook.com>
2019-07-21 13:59:39 -0400

    Add project files.

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        public string InputFilePath { get; }
+
+        public FileProcessor(string filePath)
+        {
+            this.InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            WriteLine($"Begin Process of {InputFilePath}");
+        }
+    }
+}
